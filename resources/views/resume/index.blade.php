c5694b5b3ac18681df4c7fafeb54b213ef6fd427
anasjusof <anasjusof@localhost>
2017-05-15 17:17:22 +0800

    Dummy data for printing updated

--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -1,86 +1,245 @@
+<head>
+    <style>
+        @page {
+            size: A4 potrait;
+        }
+        
+        @media print {
+            .section {page-break-after: always;}
+        }
+    </style>
+</head>
+
+
+<div class="section">
 <p><strong>Maklumat Peribadi</strong></p>
 <table width="100%" cellspacing="0" cellpadding="4">
-<tbody>
-<tr>
-<td rowspan="7" width="50%">&nbsp;</td>
-<td width="50%">
-<p>Nama</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Staff ID</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Jawatan</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Jabatan</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Fakulti</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Email</p>
-</td>
-</tr>
-<tr>
-<td width="50%">
-<p>Phone</p>
-</td>
-</tr>
-</tbody>
+    <tbody>
+        <tr valign="top">
+            <td rowspan="7" width="50%">
+                <p>&nbsp;</p>
+            </td>
+            <td width="50%">
+                <p>Nama</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Staff ID</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Jawatan</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Jabatan</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Fakulti</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Email</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="50%">
+                <p>Phone</p>
+            </td>
+        </tr>
+    </tbody>
 </table>
+<p>&nbsp;</p>
 <p><strong>Latar Belakang Pengajian</strong></p>
 <table width="100%" cellspacing="0" cellpadding="4">
-<tbody>
-<tr>
-<td width="25%">&nbsp;</td>
-<td width="25%">
-<p><u><strong>Institusi</strong></u></p>
-</td>
-<td width="25%">
-<p><u><strong>Tahap Kelulusan</strong></u></p>
-</td>
-<td width="25%">
-<p><u><strong>Nama Kelulusan</strong></u></p>
-</td>
-</tr>
-<tr>
-<td width="25%">
-<p>1</p>
-</td>
-<td width="25%">
-<p>UTeM</p>
-</td>
-<td width="25%">
-<p>Diploma</p>
-</td>
-<td width="25%">
-<p>IT</p>
-</td>
-</tr>
-<tr>
-<td width="25%">
-<p>2</p>
-</td>
-<td width="25%">
-<p>UTeM</p>
-</td>
-<td width="25%">
-<p>Sarjana Muda</p>
-</td>
-<td width="25%">
-<p>IT</p>
-</td>
-</tr>
-</tbody>
+    <tbody>
+        <tr valign="top">
+            <td width="25%">
+                <p>&nbsp;</p>
+            </td>
+            <td width="25%">
+                <p><u><strong>Institusi</strong></u></p>
+            </td>
+            <td width="25%">
+                <p><u><strong>Tahap Kelulusan</strong></u></p>
+            </td>
+            <td width="25%">
+                <p><u><strong>Nama Kelulusan</strong></u></p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="25%">
+                <p>1</p>
+            </td>
+            <td width="25%">
+                <p>UTeM</p>
+            </td>
+            <td width="25%">
+                <p>Diploma</p>
+            </td>
+            <td width="25%">
+                <p>IT</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="25%">
+                <p>2</p>
+            </td>
+            <td width="25%">
+                <p>UTeM</p>
+            </td>
+            <td width="25%">
+                <p>Sarjana Muda</p>
+            </td>
+            <td width="25%">
+                <p>IT</p>
+            </td>
+        </tr>
+    </tbody>
 </table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+
+<div class="section">
+<p><strong>Penyeliaan</strong></p>
+<table width="100%" cellspacing="0" cellpadding="4">
+    <tbody>
+        <tr valign="top">
+            <td width="17%">
+                <p>&nbsp;</p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Nama</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>No Matrik</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Tajuk</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Sem</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Status</strong></u></p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="17%">
+                <p>1</p>
+            </td>
+            <td width="17%">
+                <p>Ahmad Albab</p>
+            </td>
+            <td width="17%">
+                <p>B031210244</p>
+            </td>
+            <td width="17%">
+                <p>Fuzzy Logic</p>
+            </td>
+            <td width="17%">
+                <p>3</p>
+            </td>
+            <td width="17%">
+                <p>Lulus</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="17%">
+                <p>2</p>
+            </td>
+            <td width="17%">
+                <p>Syukri Mohd</p>
+            </td>
+            <td width="17%">
+                <p>B021213578</p>
+            </td>
+            <td width="17%">
+                <p>Image Processing</p>
+            </td>
+            <td width="17%">
+                <p>4</p>
+            </td>
+            <td width="17%">
+                <p>Gagal</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<p>&nbsp;</p>
+<hr>
+<p><strong>Pengajaran</strong></p>
+<table width="100%" cellspacing="0" cellpadding="4">
+    <tbody>
+        <tr valign="top">
+            <td width="17%">
+                <p>&nbsp;</p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Kod Kursus</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Nama Kursus</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Sem</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Bil. Pelajar</strong></u></p>
+            </td>
+            <td width="17%">
+                <p><u><strong>Tahap</strong></u></p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="17%">
+                <p>1</p>
+            </td>
+            <td width="17%">
+                <p>DITS</p>
+            </td>
+            <td width="17%">
+                <p>Information Security</p>
+            </td>
+            <td width="17%">
+                <p>3</p>
+            </td>
+            <td width="17%">
+                <p>33</p>
+            </td>
+            <td width="17%">
+                <p>Diploma</p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <td width="17%">
+                <p>2</p>
+            </td>
+            <td width="17%">
+                <p>BACS</p>
+            </td>
+            <td width="17%">
+                <p>Computer Science</p>
+            </td>
+            <td width="17%">
+                <p>4</p>
+            </td>
+            <td width="17%">
+                <p>17</p>
+            </td>
+            <td width="17%">
+                <p>Sarjana Muda</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<p>&nbsp;</p>
+</div>
+</body>
